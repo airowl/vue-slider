1ccc8d6a2f0d918b98447159cd9b8c3056950daa
rowl <airowl.gasga@gmail.com>
2022-03-17 14:34:28 +0100

    inizializzato vue js nei file html e js

--- js/script.js
+++ js/script.js
@@ -23,6 +23,44 @@ E se volessi un bottone per invertire la "direzione" del carosello?
 //* - title immagini
 //* - description
 
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            carouselElements: [
+                {
+                    image: "1.jpg",
+                    title: "THE DEVELOPER",
+                    description: "Fa tante cose FIGHE che non capisce"
+                },
+                {
+                    image: "2.jpg",
+                    title: "THE CODE",
+                    description: "Che manco io capisco"
+                },
+                {
+                    image: "3.jpg",
+                    title: "THE SCRIVANIA",
+                    description: "Mi serve solo per appoggiare il pc e il cibo che mangio"
+                },
+                {
+                    image: "4.jpg",
+                    title: "THE OCCHIALI",
+                    description: "Mi servono per vedere non per annusare"
+                },
+                {
+                    image: "5.jpg",
+                    title: "THE CODE, again",
+                    description: "Questo lo copiato, Grazie stackoverflow"
+                },
+            ]
+        },
+        methods: {
+
+        }
+    }
+);
+
 const carouselElements = [
     {
         image: "1.jpg",
@@ -51,79 +89,79 @@ const carouselElements = [
     }
 ];
 
-//! creare il codice per inserire gli elementi all'interno del DOM dell'id "my-before-carousel"
-let elementsDom = "";
-let elementsDomThumb = "";
-for (let i = 0; i < carouselElements.length; i++) {
-    elementsDom += addElements(carouselElements[i].image, carouselElements[i].title, carouselElements[i].description);
-    elementsDomThumb += addElementsThumb(carouselElements[i].image);
-}
+// //! creare il codice per inserire gli elementi all'interno del DOM dell'id "my-before-carousel"
+// let elementsDom = "";
+// let elementsDomThumb = "";
+// for (let i = 0; i < carouselElements.length; i++) {
+//     elementsDom += addElements(carouselElements[i].image, carouselElements[i].title, carouselElements[i].description);
+//     elementsDomThumb += addElementsThumb(carouselElements[i].image);
+// }
 
-//! inserire gli elementi creati all'interno del DOM  di "my-before-carousel"
-document.querySelector('.my-carousel-images').innerHTML += elementsDom;
+// //! inserire gli elementi creati all'interno del DOM  di "my-before-carousel"
+// document.querySelector('.my-carousel-images').innerHTML += elementsDom;
 
-//! inserire gli elementi creati all'interno del DOM di "my-thumbnails"
-document.querySelector('.my-thumbnails').innerHTML += elementsDomThumb;
+// //! inserire gli elementi creati all'interno del DOM di "my-thumbnails"
+// document.querySelector('.my-thumbnails').innerHTML += elementsDomThumb;
 
-//! Devo inserire la class active al primo elemento per far si che si veda
-let activeElement = 0;
+// //! Devo inserire la class active al primo elemento per far si che si veda
+// let activeElement = 0;
 
-const boxItems = document.querySelectorAll('.box');
+// const boxItems = document.querySelectorAll('.box');
 
-boxItems[activeElement].classList.add('active');
+// boxItems[activeElement].classList.add('active');
 
-//! inserimento della classe active al primo thumbnail image
-const boxItemsThumb = document.querySelectorAll('.thumbnails-img');
+// //! inserimento della classe active al primo thumbnail image
+// const boxItemsThumb = document.querySelectorAll('.thumbnails-img');
 
-boxItemsThumb[activeElement].classList.add('active-thumb');
+// boxItemsThumb[activeElement].classList.add('active-thumb');
 
-//! inserire degli event listener sui bottoni left e right
-document.querySelector('.my-previous').addEventListener('click', function() {
+// //! inserire degli event listener sui bottoni left e right
+// document.querySelector('.my-previous').addEventListener('click', function() {
 
-    elementsPrev();
+//     elementsPrev();
 
-    clearInterval(timeGo);
-    clearInterval(timeBack);
-});
+//     clearInterval(timeGo);
+//     clearInterval(timeBack);
+// });
 
-document.querySelector('.my-next').addEventListener('click', function() {
+// document.querySelector('.my-next').addEventListener('click', function() {
 
-    elementsNext();
+//     elementsNext();
 
-    clearInterval(timeGo);
-    clearInterval(timeBack);
+//     clearInterval(timeGo);
+//     clearInterval(timeBack);
 
-});
+// });
 
-//!! set interval part
-document.getElementById('my-after-carousel').classList.add('d-flex', 'justify-content-evenly');
+// //!! set interval part
+// document.getElementById('my-after-carousel').classList.add('d-flex', 'justify-content-evenly');
 
-document.getElementById('my-after-carousel').innerHTML = `
-<button type="button" class="btn btn-success" id="time-left">Time Left</button>
-<button type="button" class="btn btn-warning" id="time-right">Time Right</button>
-`;
+// document.getElementById('my-after-carousel').innerHTML = `
+// <button type="button" class="btn btn-success" id="time-left">Time Left</button>
+// <button type="button" class="btn btn-warning" id="time-right">Time Right</button>
+// `;
 
-let timeGo;
+// let timeGo;
 
-document.querySelector('#time-right').addEventListener('click', function() {
+// document.querySelector('#time-right').addEventListener('click', function() {
     
-    clearInterval(timeBack);
+//     clearInterval(timeBack);
 
-    const timeForNext = setInterval(elementsNext, 1000);
+//     const timeForNext = setInterval(elementsNext, 1000);
 
-    timeGo = timeForNext
-});
+//     timeGo = timeForNext
+// });
 
-let timeBack;
+// let timeBack;
 
-document.querySelector('#time-left').addEventListener('click', function() {
+// document.querySelector('#time-left').addEventListener('click', function() {
     
-    clearInterval(timeGo);
+//     clearInterval(timeGo);
 
-    const timeForPrev = setInterval(elementsPrev, 1000);
+//     const timeForPrev = setInterval(elementsPrev, 1000);
 
-    timeBack = timeForPrev
-});
+//     timeBack = timeForPrev
+// });
 
 
 //?@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@FUNCTIONS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
